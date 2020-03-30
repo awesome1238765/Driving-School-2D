f48dec387dfaca341580b09ab132288826294e74
awesome1238765 <awesomecool237@gmail.com>
2020-03-30 13:39:24 +0000

    Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let wasdCommand = true
 let arrowCommand = false
 let b1
-let speed = 1
 let carX = 200
 let carY = 200
 let inp1
+let s1
 function setup() {
   createCanvas(400, 400)
   rect(carX, carY, 15, 15)
@@ -12,6 +12,7 @@ function setup() {
   b1.mousePressed(changeControls)
   inp1 = createColorPicker("Put In A Certain Color For Shape")
   setMidShade();
+  s1 = createSlider(1,100,1)
 }
 function draw() {
   background(0)
@@ -20,33 +21,35 @@ function draw() {
 }
 function keyTyped() {
   if (wasdCommand == true) {
+    let speed1 = s1.value()
     if (key === 'd') {
-      carX = carX + speed
+      carX = carX + speed1
       rect(carX, carY, 15, 15)
     } else if (key === 'a') {
-      carX = carX + -speed
+      carX = carX + -speed1
       rect(carX, carY, 15, 15)
     } else if (key === 's') {
-      carY = carY + speed
+      carY = carY + speed1
       rect(carX, carY, 15, 15)
     } else if (key === 'w') {
-      carY = carY + -speed
+      carY = carY + -speed1
       rect(carX, carY, 15, 15)
     }
   }
 }
 function keyPressed() {
   if (arrowCommand == true) {
+    let speed2 = s1.value()
     if (keyCode === LEFT_ARROW) {
-      carX = carX + -1
+      carX = carX + -speed2
       rect(carX, carY, 15, 15)
     }else if (keyCode === RIGHT_ARROW){
-      carX = carX + 1
+      carX = carX + speed2
     }else if(keyCode === UP_ARROW){
-      carY = carY + -1
+      carY = carY + -speed2
       rect(carX, carY, 15, 15)
     }else if(keyCode === DOWN_ARROW){
-      carY = carY + 1
+      carY = carY + speed2
       rect(carX, carY, 15, 15)
     }
   }
@@ -68,5 +71,4 @@ function setMidShade() {
 
 function setShade1() {
   setMidShade();
-  console.log('You are choosing shade 1 to be : ', this.value());
 }
